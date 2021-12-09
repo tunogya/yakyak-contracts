10b7049811e852855150d1ab34d8f75d77fa2d4f
tunogya <tunogya@qq.com>
2021-12-09 23:06:05 +0800

    use ERC20

--- contracts/Faucet.sol
+++ contracts/Faucet.sol
@@ -2,13 +2,13 @@
 pragma solidity ^0.8.2;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Fault is Ownable {
-    IERC20 public token;
+    ERC20 public token;
 
     constructor (address tokenAddress) {
-        token = IERC20(tokenAddress);
+        token = ERC20(tokenAddress);
     }
 
     // Withdraw Yak from contract
