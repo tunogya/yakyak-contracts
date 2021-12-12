a7cb1be7fbae3c039a5659d5d4208d556c9f7af1
tunogya <tunogya@qq.com>
2021-12-12 13:56:13 +0800

    update YakYakBank.sol

--- contracts/YakYakBank.sol
+++ contracts/YakYakBank.sol
@@ -6,7 +6,7 @@ pragma solidity ^0.8.2;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 interface IYakYakBank {
-    // cashing cheque order
+    // cash cheque order
     struct ORDER {
         uint256 amount;
         address casher;
@@ -18,86 +18,87 @@ interface IYakYakBank {
         uint256 amount;
     }
 
-    event Withdraw(address account, uint256 amount);
-    event Deposit(address account, uint256 amount);
+    event Withdraw(address indexed account, uint256 amount);
+    event Deposit(address indexed account, uint256 amount);
+    event Cash(address indexed from, uint256 id, uint256 amount, address indexed casher);
 
     // Get the balance of account
-    function balanceOf(address _account) external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
     // Get EIP712 domain
-    function getEIP712Domain() external view returns(string memory _name, string memory _version, uint256 _chainid, address _verifyingContract, bytes32 _salt);
+    function getEIP712Domain() external view returns(string memory name, string memory version, uint256 chainid, address verifyingContract, bytes32 salt);
     // Deposit token into contract
-    function deposit(uint256 _amount) external;
+    function deposit(uint256 amount) external;
     // Withdraw token from contract
-    function withdraw(address _to, uint256 _amount) external;
+    function withdraw(address to, uint256 amount) external;
     // Get the cashing cheque
-    function getOrder(address _account, uint256 _id) external view returns (ORDER memory);
+    function getOrder(address account, uint256 id) external view returns (ORDER memory);
     // Hash cheque
-    function hashCheque(CHEQUE memory _cheque) external view returns (bytes32);
+    function hashCheque(CHEQUE memory cheque) external view returns (bytes32);
     // Verify cheque
-    function verify(CHEQUE memory _cheque, uint256 _signature) external view returns(address);
-    // Cashing cheque
-    function cashing(CHEQUE memory _cheque, uint256 _signature) external;
+    function verify(CHEQUE memory cheque, bytes32 r, bytes32 s, uint8 v) external view returns(address);
+    // Cash cheque
+    function cash(CHEQUE memory cheque, bytes32 r, bytes32 s, uint8 v) external;
 }
 
 contract YakYakBank is IYakYakBank {
     // The token to be cashed
-    ERC20 public token;
+    ERC20 public _token;
 
     // Used to EIP712 domain
-    string name;
-    string version;
-    uint256 chainid;
-    address verifyingContract;
-    bytes32 salt;
+    string _name;
+    string _version;
+    uint256 _chainid;
+    address _verifyingContract;
+    bytes32 _salt;
 
-    constructor (address _tokenAddress, string memory _name, string memory _version, bytes32 _salt) {
-        token = ERC20(_tokenAddress);
-        name = _name;
-        version = _version;
-        chainid = block.chainid;
-        verifyingContract = address(this);
-        salt = _salt;
+    constructor (address tokenAddress_, string memory name_, string memory version_, bytes32 salt_) {
+        _token = ERC20(tokenAddress_);
+        _name = name_;
+        _version = version_;
+        _chainid = block.chainid;
+        _verifyingContract = address(this);
+        _salt = salt_;
     }
 
     // Get EIP712 domain
-    function getEIP712Domain() public override view returns(string memory _name, string memory _version, uint256 _chainid, address _verifyingContract, bytes32 _salt) {
-        _name = name;
-        _version = version;
-        _chainid = chainid;
-        _verifyingContract = verifyingContract;
-        _salt = salt;
+    function getEIP712Domain() public override view returns(string memory name, string memory version, uint256 chainid, address verifyingContract, bytes32 salt) {
+        name = _name;
+        version = _version;
+        chainid = _chainid;
+        verifyingContract = _verifyingContract;
+        salt = _salt;
     }
 
     // Map of all users' balance
-    mapping(address => uint256) ledger;
+    mapping(address => uint256) _ledger;
 
     // Get the balance of account
-    function balanceOf(address _account) public override view returns (uint256) {
-        return ledger[_account];
+    function balanceOf(address account) public override view returns (uint256) {
+        return _ledger[account];
     }
 
     // Deposit token into contract
-    function deposit(uint256 _amount) public override {
-        require(_amount <= token.balanceOf(msg.sender), "Bank: Sorry, your balance is running low!");
-        token.transfer(address(this), _amount);
-        ledger[msg.sender] += _amount;
-        emit Deposit(msg.sender, _amount);
+    function deposit(uint256 amount) public override {
+        require(amount <= _token.balanceOf(msg.sender), "Bank: Sorry, your balance is running low!");
+        _token.transfer(address(this), amount);
+        _ledger[msg.sender] += amount;
+        emit Deposit(msg.sender, amount);
     }
 
     // Withdraw token from user's balance
-    function withdraw(address _to, uint256 _amount) public override {
-        require(_amount <= ledger[msg.sender], "Bank: Sorry, your balance is running low!");
-        ledger[msg.sender] -= _amount;
-        token.transferFrom(address(this), _to, _amount);
-        emit Withdraw(msg.sender, _amount);
+    function withdraw(address to, uint256 amount) public override {
+        require(amount <= _ledger[msg.sender], "Bank: Sorry, your balance is running low!");
+        _ledger[msg.sender] -= amount;
+        _token.transferFrom(address(this), to, amount);
+        emit Withdraw(msg.sender, amount);
     }
 
     // Map of all users' cashing cheque orders
-    mapping(address => mapping(uint256 => ORDER)) orders;
+    mapping(address => mapping(uint256 => ORDER)) _orders;
 
     // Get the cashing cheque order
-    function getOrder(address _account, uint256 _id) public override view returns (ORDER memory) {
-        return orders[_account][_id];
+    function getOrder(address account, uint256 id) public override view returns (ORDER memory) {
+        return _orders[account][id];
     }
 
     // The typehash of CHEQUE
@@ -108,38 +109,39 @@ contract YakYakBank is IYakYakBank {
 
     bytes32 public DOMAIN_SEPARATOR = keccak256(abi.encode(
         EIP712_DOMAIN_TYPEHASH,
-        keccak256(abi.encode(name)),
-        keccak256(abi.encode(version)),
-        chainid,
-        verifyingContract,
-        salt
+        keccak256(abi.encode(_name)),
+        keccak256(abi.encode(_version)),
+        _chainid,
+        _verifyingContract,
+        _salt
     ));
 
     // Hash cheque
-    function hashCheque(CHEQUE memory _cheque) public override view returns (bytes32) {
+    function hashCheque(CHEQUE memory cheque) public override view returns (bytes32) {
         return keccak256(abi.encodePacked(
             "\x19\x01",
             DOMAIN_SEPARATOR,
             keccak256(abi.encode(
                 CHEQUE_TYPEHASH,
-                _cheque.id,
-                _cheque.amount
+                cheque.id,
+                cheque.amount
             ))
         ));
     }
 
     // Verify cheque
-    function verify(CHEQUE memory _cheque, uint256 _signature) public override view returns(address) {
-        // @Todo encode r, s, v from _signature
-        bytes32 r;
-        bytes32 s;
-        uint8 v;
-
-        return ecrecover(hashCheque(_cheque), v, r, s);
+    function verify(CHEQUE memory cheque, bytes32 r, bytes32 s, uint8 v) public override view returns(address) {
+        return ecrecover(hashCheque(cheque), v, r, s);
     }
 
-    function cashing(CHEQUE memory _cheque, uint256 _signature) public override{
-        address signer = verify(_cheque, _signature);
-        require(_cheque.amount <= ledger[signer], "Bank: Sorry, this is a dishonored check!");
+    // Cash cheque
+    function cash(CHEQUE memory cheque, bytes32 r, bytes32 s, uint8 v) public override{
+        address signer = verify(cheque, r, s, v);
+        require(cheque.amount <= _ledger[signer], "Bank: Sorry, this is a dishonored check!");
+        require(_orders[signer][cheque.id].casher == address(0), "Bank: Sorry, this check is void!");
+        _ledger[signer] -= cheque.amount;
+        _orders[signer][cheque.id] = ORDER(cheque.amount, msg.sender);
+        _token.transferFrom(address(this), msg.sender, cheque.amount);
+        emit Cash(signer, cheque.id, cheque.amount, msg.sender);
     }
 }
