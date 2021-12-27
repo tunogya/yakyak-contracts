5e2529552847c37e7d1844a6fc4e6622592e05ce
tunogya <tunogya@qq.com>
2021-12-28 00:30:35 +0800

    update YakYakMe

--- contracts/YakYakMe.sol
+++ contracts/YakYakMe.sol
@@ -4,25 +4,13 @@
 pragma solidity ^0.8.2;
 
 contract YakYakMe {
-    mapping(address => bytes32) private addressToName;
-    mapping(bytes32 => address) private nameToAddress;
+    mapping(address => bytes32) public addressToName;
+    mapping(bytes32 => address) public nameToAddress;
 
     event Take(address indexed account, bytes32 indexed name);
     event Update(address indexed account, bytes32 indexed name);
 
     function take(bytes32 nameBytes) public {
-        require(addressToName[msg.sender] == 0, "YakYakMe: your already has a username!");
-        require(nameToAddress[nameBytes] == address(0), "YakYakMe: this username has used by others!");
-        addressToName[msg.sender] = nameBytes;
-        nameToAddress[nameBytes] = msg.sender;
-        emit Take(msg.sender, nameBytes);
-    }
-
-    function take(string memory name) public {
-        bytes32 nameBytes;
-        assembly {
-            nameBytes := mload(add(name, 32))
-        }
         require(addressToName[msg.sender] == 0, "YakYakMe: your already has a username!");
         require(nameToAddress[nameBytes] == address(0), "YakYakMe: this username has used by others!");
         addressToName[msg.sender] = nameBytes;
@@ -37,34 +25,5 @@ contract YakYakMe {
         addressToName[msg.sender] = nameBytes;
         nameToAddress[nameBytes] = msg.sender;
         emit Update(msg.sender, nameBytes);
-    }
-
-    function update(string memory name) public {
-        bytes32 nameBytes;
-        assembly {
-            nameBytes := mload(add(name, 32))
-        }
-        require(addressToName[msg.sender] != 0, "YakYakMe: you has no username");
-        require(nameToAddress[addressToName[msg.sender]] == msg.sender, "YakYakMe: this isn't your username!");
-        nameToAddress[addressToName[msg.sender]] = address(0);
-        addressToName[msg.sender] = nameBytes;
-        nameToAddress[nameBytes] = msg.sender;
-        emit Update(msg.sender, nameBytes);
-    }
-    
-    function fetchName(address account) public view returns (string memory) {
-        return string(abi.encodePacked(addressToName[account]));
-    }
-    
-    function fetchAddress(bytes32 nameBytes) public view returns (address) {
-        return nameToAddress[nameBytes];
-    }
-
-    function fetchAddress(string memory name) public view returns (address) {
-        bytes32 nameBytes;
-        assembly {
-            nameBytes := mload(add(name, 32))
-        }
-        return nameToAddress[nameBytes];
     }
 }
