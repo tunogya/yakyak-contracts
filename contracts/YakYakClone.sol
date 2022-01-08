6b54b987c1348ec26170ee7e4f7ce51f61c49db6
tunogya <tunogya@qq.com>
2022-01-09 01:15:15 +0800

    update

--- contracts/YakYakClone.sol
+++ contracts/YakYakClone.sol
@@ -58,7 +58,7 @@ contract YakYakClone is ERC721, ERC721Burnable, Ownable {
     mapping(uint32 => uint32) numberMintedPerDNA;
   }
 
-  function totalSupply() public view virtual override returns (uint256) {
+  function totalSupply() public view returns (uint256) {
     return _nextCloneID;
   }
 
