b3c18b5b80467b1439b7063ae632ba0844750556
tunogya <tunogya@qq.com>
2022-02-10 23:41:04 +0800

    update

--- scripts/deployYNS@rinkeby.ts
+++ scripts/deployYNS@rinkeby.ts
@@ -19,21 +19,21 @@ async function main() {
   const accounts = signers.map((s: SignerWithAddress) => s.address);
   const ens = await ENSRegistry.deploy();
   await ens.deployed();
-  console.log("ens: ", ens.address);
+  console.log("ens: ", ens.address); // 0x8A789CC874C89BCE9bf5B0f909752196d8CfE830
   const resolver = await PublicResolver.deploy(ens.address, ZERO_ADDRESS);
   await resolver.deployed();
-  console.log("resolver: ", resolver.address);
+  console.log("resolver: ", resolver.address); // 0x92ABfC191362ADa53f37104Ce9D9DB5A59a3d14b
   await setupResolver(ens, resolver, accounts);
   const registrar = await FIFSRegistrar.deploy(ens.address, namehash.hash(tld));
   await registrar.deployed();
-  console.log("registrar: ", registrar.address);
+  console.log("registrar: ", registrar.address); // 0xaCAf3212Cf678c9BfBf8050A73E26752E8Acea80
   await setupRegistrar(ens, registrar);
   const reverseRegistrar = await ReverseRegistrar.deploy(
     ens.address,
     resolver.address
   );
   await reverseRegistrar.deployed();
-  console.log("reverseRegistrar: ", reverseRegistrar.address);
+  console.log("reverseRegistrar: ", reverseRegistrar.address); // 0x314e46Ee3f5519f6f159A2962bc8e36794d73700
   await setupReverseRegistrar(ens, registrar, reverseRegistrar, accounts);
 }
 // @ts-ignore
