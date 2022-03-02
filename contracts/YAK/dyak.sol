3cafbb372024d3de3785a642e12742dcb2d38e5a
tunogya <tunogya@qq.com>
2022-03-02 12:11:56 +0800

    new version

--- contracts/YAK/dyak.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity ^0.8.4;
-
-import {INameWrapper, PublicResolver} from '@ensdomains/ens-contracts/contracts/resolvers/PublicResolver.sol';
-import '@ensdomains/ens-contracts/contracts/registry/ENSRegistry.sol';
-import '@ensdomains/ens-contracts/contracts/registry/FIFSRegistrar.sol';
-import {NameResolver, ReverseRegistrar} from '@ensdomains/ens-contracts/contracts/registry/ReverseRegistrar.sol';
